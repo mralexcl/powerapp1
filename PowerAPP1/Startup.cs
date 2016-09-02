eba314e0351711724629140a158495983f3d958a
Alex Campos Arcos <alex_campos_arcos@hotmail.com>
2016-09-02 15:09:10 -0300

    Add project files.

---
+++ PowerAPP1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PowerAPP1.Startup))]
+namespace PowerAPP1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
