8a4256c5b091738d555c507a5e6ae9d8507f24b6
Alex Campos Arcos <alex_campos_arcos@hotmail.com>
2016-09-02 15:09:51 -0300

    initial commit

--- PowerAPP1/Controllers/HomeController.cs
+++ PowerAPP1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace PowerAPP1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page 1.";
 
             return View();
         }
